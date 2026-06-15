19b6624c50bee701c96f23941e10b78a3e9b1a1b
shashibaranwal <shashibaranwal09@gmail.com>
2026-06-15 18:22:57 +0545

    setup firebase

---
+++ src/services/firebase.js
@@ -0,0 +1,19 @@
+import { initializeApp } from "firebase/app";
+import { getDatabase } from "firebase/database";
+import dotenv from 'dotenv';
+
+
+
+const firebaseConfig = {
+  apiKey: import.meta.env.VITE_FB_API_KEY,
+  authDomain: import.meta.env.VITE_FB_AUTH_DOMAIN,
+  databaseURL: import.meta.env.VITE_FB_DATABASE_URL,
+  projectId: import.meta.env.VITE_FB_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FB_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FB_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FB_APP_ID,
+};
+
+
+const app = initializeApp(firebaseConfig);
+export const database = getDatabase(app);
