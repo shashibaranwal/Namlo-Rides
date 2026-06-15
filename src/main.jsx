29983bb1ce947d7df63c8f7a6cf65d0ba9e87936
shashibaranwal <shashibaranwal09@gmail.com>
2026-06-15 20:16:41 +0545

    fixing imports

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+
+import L from "leaflet";
+import marker from "leaflet/dist/images/marker-icon.png";
+import marker2x from "leaflet/dist/images/marker-icon-2x.png";
+import shadow from "leaflet/dist/images/marker-shadow.png";
+
+L.Icon.Default.mergeOptions({ iconUrl: marker, iconRetinaUrl: marker2x, shadowUrl: shadow });
+
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
